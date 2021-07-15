a1de93177d170625adcf574e1be4088df94534ca
Yazmin Tinoco <ytip30@gmail.com>
2021-07-14 22:50:58 -0500

    Update workouts adding the new workouts

--- controllers/api/workoutRoutes.js
+++ controllers/api/workoutRoutes.js
@@ -13,30 +13,40 @@ router.get("/", (req, res) => {
 // Get info for the range page
 router.get("/range", (req, res) => {
     db.Workout.find()
-    .then((dbWorkout) => {
-        res.json(dbWorkout);
-    }).catch(err => {
-        res.status(400).json(err);
-    });
+        .then((dbWorkout) => {
+            res.json(dbWorkout);
+        }).catch(err => {
+            res.status(400).json(err);
+        });
 });
 // Create a new completed workouts
 router.post("/", (req, res) => {
     db.Workout.create(req.body)
-    .then((dbWorkout) => {
-        res.json(dbWorkout);
-    }).catch(err => {
-        res.status(400).json(err);
-    });
+        .then((dbWorkout) => {
+            res.json(dbWorkout);
+        }).catch(err => {
+            res.status(400).json(err);
+        });
 });
-// Update workouts by MongoDB _id value and update the exercsise body
+// Update workouts adding the new workouts
 router.put("/:id", (req, res) => {
-    db.Workout.findByIdAndUpdate(
-        { _id: req.params.id }, { exercises: req.body }
-    ).then((dbWorkout) => {
-        res.json(dbWorkout);
-    }).catch(err => {
-        res.status(400).json(err);
-    });
+    const id = req.params.id;
+    let workoutExercises = [];
+    db.Workout.find({ _id: id })
+        .then(dbWorkout => {
+            workoutExercises = dbWorkout[0].exercises;
+            res.json(workoutExercises);
+            let exercises = [...workoutExercises, req.body];
+            db.Workout.findByIdAndUpdate(id, { exercises: exercises }, function (err, doc) {
+                if (err) {
+                    console.log(err)
+                }
+            })
+        })
+        .catch(err => {
+            res.json(err);
+        });
+
 });
 
 module.exports = router;
