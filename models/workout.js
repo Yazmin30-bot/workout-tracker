a1d1edf01725bd8b6d05fd3669be092d44d6bafa
Yazmin Tinoco <ytip30@gmail.com>
2021-07-14 22:34:29 -0500

    Update model workout add Exercises array

--- models/workout.js
+++ models/workout.js
@@ -7,11 +7,40 @@ const WorkoutSchema = new Schema({
         default: Date.now
     },
     exercises: [
+
         {
-            type: Schema.Types.ObjectId,
-            ref: "Exercise"
+
+            name: {
+                type: String,
+                trim: true,
+                required: true
+            },
+            type: {
+                type: String,
+                trim: true,
+                required: true
+            },
+            weight: {
+                type: Number,
+            },
+            sets: {
+                type: Number,
+            },
+            reps: {
+                type: Number,
+            },
+            duration: {
+                type: Number,
+                required: true
+            },
+            distance: {
+                type: Number
+            },
+
         }
+
     ],
+
 });
 
 const Workout = mongoose.model("Workout", WorkoutSchema);
